6bf9cf7408d1eb00433bd65f0e4ea06f17dbc860
kennby <153956421+kennby@users.noreply.github.com>
2024-01-16 17:27:47 -0500

    Update ai-bard.js

--- plugins/ai-bard.js
+++ plugins/ai-bard.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 
 var handler = async (m, { text,  usedPrefix, command }) => {
 
-if (!text) return conn.reply(m.chat, `🎌 *Ingrese una petición*\n\nEjemplo, !bard Conoces conoces onyx bot?`, m, fake, )
+if (!text) return conn.reply(m.chat, `❤️ *Ingrese una petición*\n\nEjemplo, !bard Conoces conoces onyx bot?`, m, fake, )
 
 try {
 
