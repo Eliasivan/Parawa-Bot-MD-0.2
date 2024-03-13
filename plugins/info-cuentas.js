5aaaca37f8fd9c5dd7de0b234aab9489e61366e9
Eliasivan <159085113+Eliasivan@users.noreply.github.com>
2024-03-13 13:34:28 -0300

    Actualizar info-cuentas.js

--- plugins/info-cuentas.js
+++ plugins/info-cuentas.js
@@ -1,6 +1,6 @@
 let media = [imagen1, imagen2].getRandom()
 let handler = async (m, { conn, command }) => {
-let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+let fkontak = { "key": { "participants":"", "remoteJid": "", "fromMe": false, "id": "Halo" }, "message": { "": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "" }
 let str = `
 ┏───◆ CUENTAS ◆───┓
 │ *GITHUB*
